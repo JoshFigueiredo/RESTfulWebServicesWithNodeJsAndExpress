5b9280f3fefe01ffb4390b86323e1b2fc14d8345
JoshFigueiredo <joshfigueiredo@gmail.com>
2018-02-07 20:25:56 -0500

    Completed this course

--- exercises/app.js
+++ exercises/app.js
@@ -1,13 +1,33 @@
 var express = require('express'),
-    mongoose = require('mongoose');
-    bodyParser = require('body-parser');
-    
-var dbServer = 'localhost';
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    fs = require('fs');
 
-if (process.env.ENV == 'Test')
-    db = mongoose.connect('mongodb://' + dbServer + '/bookAPI_test');
-else   
-    db = mongoose.connect('mongodb://' + dbServer + '/bookAPI');
+var dbServer;
+fs.readFile('../../DBServer.json', 'utf-8', function(err, data){
+    // console.log(data);
+    // var obj = JSON.parse(data);
+    
+    // dbServer = obj.dbServer;
+    // console.log(dbServer)
+    if (err)
+        {
+            console.log('File not found. Defaulting dbServer to localhost');
+            dbServer = 'localhost';
+        }
+    else{
+        var obj = JSON.parse(data);
+        dbServer = obj.dbServer;
+    }
+
+    // Connect to DB
+    if (process.env.ENV == 'Test')
+        db = mongoose.connect('mongodb://' + dbServer + '/bookAPI_test');
+    else   {
+        console.log('DBSERVER:    ' + dbServer);
+        db = mongoose.connect('mongodb://' + dbServer + '/bookAPI');
+    }
+})
 
 var Book = require('./models/bookModel');
 
@@ -29,5 +49,6 @@ app.get('/', function(req, res){
 
 app.listen(port, function(){
     console.log('Gulp is running my app on port ' + port)
-    console.log('dbServer: ' + JSON.stringify(dbServer))
 })
+
+module.exports = app;
